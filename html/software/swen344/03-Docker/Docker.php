97a117b1a13162a5002b13de26012855235e2bd9
Aidan Sawyer <aks5238@rit.edu>
2016-02-21 01:03:50 -0500

    Docker presentation slider bugfixed and working properly.

--- html/software/swen344/03-Docker/Docker.php
+++ html/software/swen344/03-Docker/Docker.php
@@ -1,7 +1,7 @@
 <html>
   <title>Docker</title>
   
-  <?php include_once("../../../../../includes/header.php"); ?>
+  <?php include_once("../../../../includes/header.php"); ?>
   
   <h2>Docker</h2>
   <h3>Aidan Sawyer - atla5@dockerhub</h3>
@@ -12,11 +12,10 @@
   <!-- Indicators -->
   <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    
+
     <?php 
-      for($i=1;i<=15;i++){
-        echo '<li data-target="#myCarousel" data-slide-to=';
-        echo "$i</li>";
+      for($i=1;$i<=15;$i++){
+        echo "<li data-target=\"#myCarousel\" data-slide-to=$i></li>";
       }
     ?>
     
@@ -25,14 +24,12 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="./DockerPresentationSlides/DockerPresentation(0)">
+      <img src="./DockerPresentationSlides/DockerPresentation(0).png">
     </div>
 
     <?php 
-      for($i=1;i<=15;i++){
-        echo '<div class="item"> <img src="./DockerPresentationSlides/DockerPresentation(';
-        echo "$i";
-        echo ').png"> </div>';
+      for($i=1;$i<=15;$i++){
+        echo "<div class=\"item\"> <img src=\"./DockerPresentationSlides/DockerPresentation($i).png\"> </div>";
       }
     ?>
 
@@ -55,5 +52,5 @@
   
   <!--insert links to references-->
   
-  <?php include_once("../../../../../includes/footer.php"); ?>
+  <?php include_once("../../../../includes/footer.php"); ?>
 </html>
