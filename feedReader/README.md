63023144bf069c32982f623c7adb547a05ea2a00
Aidan Sawyer <aidan.sawyer@ithaka.org>
2017-07-25 18:25:34 -0400

    rm -rf 'feedReader' for now

--- feedReader/README.md
+++
@@ -1,2 +0,0 @@
-# FeedReader
-Single page web app that allows user to selectively determine RSS feeds displayed from category litsting
--- feedReader/js/selector-pane.js
+++
@@ -1,9 +0,0 @@
-var lsCbTypes  = [ document.getElementById("cbArticles"), document.getElementById("cbPodcast"), document.getElementById("cbBlogs") ];
-var lsCbTopics = [ document.getElementById("cbLibraryScience"), document.getElementById("cbPhilosophy"), 
-  document.getElementById("cbNews"), document.getElementById("cbSports") ];
-
-$.('#cbAllTopics').change( function(){
-  if(this.checked) {
-        //Do stuff
-    }
-});
