08c77120e1621651aa86fa04c77609ec2206f602
Aidan Sawyer <aidan.sawyer@ithaka.org>
2017-07-26 19:56:43 -0400

    initial boilerplate of routing.js with 'context' from /res/resume

--- js/routing.js
+++ js/routing.js
@@ -1,60 +1,79 @@
 /* get 'page' variable from url  note: FROM css-tricks.com */
-function getParameterValueFromURL(string)
-{
-     var query = window.location.search.substring(1);
-     var vars = query.split("&");
-     for (var i=0;i<vars.length;i++) {
-             var pair = vars[i].split("=");
-             if(pair[0] == string){return pair[1];}
-     }
-     return(false);
+function getParameterValueFromURL(string) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == string) {
+            return pair[1];
+        }
+    }
+    return (false);
 }
 
 /* load given 'page' into the main_content section */
-function loadContent(){
+function loadContent() {
 
-  // -
-  var page = getParameterValueFromURL('page');
+    // - set variables from url
+    var page = getParameterValueFromURL('page');
+    var project_name = getParameterValueFromURL('name');
 
-  // - set defoult values for the files we want to load - //
-  var content = ""; //, leftPane = "";
+    // - set default values for the files we want to load - //
+    var template = ""; //, leftPane = "";
+    var context = {};
 
-  // - set these content
-  switch(page){
-    case "about":
-      content = "includes/under-construction.html";  //"pages/about.html";
-      break;
-    case "project-detail":
-      content = "pages/project-detail.html";
-      break;
-    case "projects":
-      content = "pages/projects.html";
-      break;
-    case "feeds":
-      content = "includes/under-construction.html"; //"pages/feeds.html";
-      break;
-    case "blog":
-      content = "includes/under-construction.html"; //"pages/blog.html";
-      break;
-    default:
-      content = "pages/home.html";
-  }
+    additional = loadJSON("/res/resume/additional.json");
+    projects = loadJSON("res/resume/projects.json")
 
-  //load selected content into #content section of index.html
-  $(document).ready(function(){
-    $('#main_content').load(content);
-  });
+    // - use handlebar and helpers to render template with given context
+    switch (page) {
+        case "resume":
+            template = "includes/under-construction.html";  //"pages/resume.html";
+            context = {
+                "education": loadJSON("/res/resume/education.json"),
+                "experience": loadJSON("/res/resume/experience.json"),
+                "skills": additional.get('skills'),
+                "languages": additional.get('languages'),
+                "links": additional.get('links')
+            };
+            break;
+        case "projects":
+            template = "pages/projects.html";
+            context = projects;
+            break;
+        case "project-detail":
+            template = "pages/project-detail.html";
+            context = projects.get(project_name);
+            break;
+        case "feeds":
+            template = "includes/under-construction.html"; //"pages/feeds.html";
+            break;
+        case "blog":
+            template = "includes/under-construction.html"; //"pages/blog.html";
+            break;
+        default:
+            template = "pages/home.html";
+    }
+
+    //load selected content into #content section of index.html
+    $(document).ready(function () {
+        $('#main_content').load(template);
+    });
 }
 
 /* load header, navbar, and footer from include files */
-function initializeStaticElements(){
+function initializeStaticElements() {
 
-  console.log("initializing static elements...");
+    console.log("initializing static elements...");
 
-  $(document).ready(function(){
-    $('#main_navbar').load('includes/navbar.html');
-    $('#main_footer').load('includes/footer.html');
-  });
+    $(document).ready(function () {
+        $('#main_navbar').load('includes/navbar.html');
+        $('#main_footer').load('includes/footer.html');
+    });
 
-  console.log("static elements initialized.");
+    console.log("static elements initialized.");
+}
+
+function loadJSON(filename){
+    return {};
 }
