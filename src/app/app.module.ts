fbb120bebb348b24c70ed2c8c26277eebe69c447
atla5 <aks5238@rit.edu>
2017-10-27 01:28:33 -0400

    angular: add initial 'projects' component with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 import { ResumeService } from "./services/resume.service";
+
+const appRoutes: Routes = [
+  {path:'', component:AboutComponent},
+  {path:'about', component:AboutComponent},
+  {path:'projects', component:ProjectsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     ResumeService
