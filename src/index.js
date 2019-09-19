cbfa3b6ad7e95ba0d5a6970a24c8b396ad9c68b6
Aidan <aks5238@rit.edu>
2019-09-19 14:06:38 -0400

    wc: load data asynchronously from atla5/resume

--- src/index.js
+++ src/index.js
@@ -1,6 +1,57 @@
-import {render} from 'lit-html';
+import {render, html} from 'https://unpkg.com/lit-html/lit-html?module';
 
-// job experience
-import {data_job} from './services.js';
-import {temp_experience} from './templates.js';
-render(temp_experience(data_job), document.getElementById("jobs"));
+// store urls for grabbing data from atla5/resume
+const cors_prefix = 'https://cors-anywhere.herokuapp.com/';
+const url_base_data = cors_prefix+"https://raw.githubusercontent.com/atla5/resume/master/data/";
+
+// generic helper function used to grab the data 
+function getDataFromJSONFileAndCallRenderFunction(data_url, renderFunction) {
+  return fetch(data_url)
+    .then((resp) => resp.json())
+    .then((json) => renderFunction(json))
+    .catch(function(error) {
+      console.log(error)
+      console.log(JSON.stringify(error));
+    }
+  );
+}
+
+
+/* --  EDUCATION -- */
+let school_url = url_base_data+"education.json";
+getDataFromJSONFileAndCallRenderFunction(school_url, renderSchoolsIntoEducationSection);
+let temp_education = (school) => html`
+<div class="education">
+  <span class="underline">${school.school_name}</span>:  ${school.degree} in ${school.major}
+  <em class="right">${school.time_start} - ${school.time_end}</em>
+  <ul>${school.notes.map((note) => html`<li>${note}</li>`)}</ul>
+</div>`;
+
+async function renderSchoolsIntoEducationSection(schools){
+  render(temp_education(schools[0]), document.getElementById("education"));
+}
+
+
+/* -- EXPERIENCE -- */
+let jobs_url = url_base_data+"experience.json";
+getDataFromJSONFileAndCallRenderFunction(jobs_url, renderJobsIntoJobsSection);
+let job_template = (job) => html`
+  <li class="pbl">
+    <div>
+      <strong><span class="underline">${job.company_name}</span>: ${job.position}</strong>
+      <em class="right">${job.time_start} - ${job.time_end || "present"}</em>
+    </div>
+    <div>
+      <em>${job.advisor_name} - ${job.advisor_position} - 
+        <a href="mailto:${job.advisor_contact}">${job.advisor_contact}</a>
+      </em>
+      <ul>${job.responsibilities.map((note) => html`<li>${note}</li>`)}</ul>
+    </div>
+  </li>
+`;
+let jobsTemplate = (jobs) => html`<ul>${jobs.map((job) => job_template(job))}</ul>`;
+
+async function renderJobsIntoJobsSection(jobs){
+  render(jobsTemplate(jobs.slice(0,3)), document.getElementById("jobs"));
+};
+
--- src/templates.js
+++ src/templates.js
@@ -1,12 +1,5 @@
 import {html} from 'lit-html';
 
-export const temp_education = (school) => html`
-  <div class="education">
-    <span class="underline">${school.school_name}</span>:  ${school.degree} in ${school.major}
-    <em class="right">${school.time_start} - ${school.time_end}</em>
-    <ul>${school.notes.map((note) => html`<li>${note}</li>`)}</ul>
-  </div>`;
-
 export const temp_project = (project) => html`
   <strong><a href="${project.url_src}">${project.name}</a></strong>
   <span class="right">${project.description_short}</span>`;
@@ -22,18 +15,6 @@ export const temp_project_detail = (project) => html`
       <strong>tags:</strong> ${project.tags}
     </div>
     <hr />
-  </div>`;
-
-export const temp_experience = (job) => html`
-  <div>
-    <strong><span class="underline">${job.company_name}</span>: ${job.position}</strong>
-    <em class="right">${job.time_start} - ${job.time_end}</em>
-  </div>
-  <div>
-    <em>${job.advisor_name} - ${job.advisor_position} - 
-      <a href="mailto:${job.advisor_contact}">${job.advisor_contact}</a>
-    </em>
-    <ul>${job.responsibilities.map((note) => html`<li>${note}</li>`)}</ul>
   </div>`;
 
 
